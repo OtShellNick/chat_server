de107df4d5b6952e932d17fb1c59debe4c44394d
otshellnick <exte92@gmail.com>
2022-02-26 01:03:17 +0300

    up cors

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ require('./DB').initDBConnection();
 
 const express = require('express');
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const Server = express();
 const {PORT} = process.env;
 
 Server.use(express.json());
 Server.use(cookieParser());
+Server.use(cors())
 
 Server.use('/api/users', require('./requests/users'))
 
