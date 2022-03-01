8c2d4932ccb8296a8adfc7e8204d149b4aa4efa3
otshellnick <exte92@gmail.com>
2022-03-01 21:55:07 +0300

    up login

--- index.js
+++ index.js
@@ -5,7 +5,41 @@ require('./DB').initDBConnection();
 
 let broker = new ServiceBroker();
 
-broker.createService(ApiService);
+broker.createService({
+    mixins: [ApiService],
+
+    settings: {
+
+        // Global CORS settings for all routes
+        cors: {
+            // Configures the Access-Control-Allow-Origin CORS header.
+            origin: "*",
+            // Configures the Access-Control-Allow-Methods CORS header.
+            methods: ["GET", "OPTIONS", "POST", "PUT", "DELETE"],
+            // Configures the Access-Control-Allow-Headers CORS header.
+            allowedHeaders: [],
+            // Configures the Access-Control-Expose-Headers CORS header.
+            exposedHeaders: [],
+            // Configures the Access-Control-Allow-Credentials CORS header.
+            credentials: false,
+            // Configures the Access-Control-Max-Age CORS header.
+            maxAge: 3600
+        },
+
+        routes: [{
+            path: "/api",
+            aliases: {
+                'POST /user/login': 'user.login'
+            },
+            // Route CORS settings (overwrite global settings)
+            cors: {
+                origin: ["http://localhost:3701", "https://localhost:5402"],
+                methods: ["GET", "OPTIONS", "POST"],
+                credentials: true
+            },
+        }]
+    }
+});
 
 broker.start();
 
