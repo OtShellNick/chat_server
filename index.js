6e538ff5fe8cc16dab1f2a15d909b6002e4f5158
otshellnick <exte92@gmail.com>
2022-03-01 21:32:32 +0300

    up login

--- index.js
+++ index.js
@@ -1,5 +1,45 @@
 require('dotenv').config();
+let { ServiceBroker } = require("moleculer");
+let ApiService = require("moleculer-web");
 require('./DB').initDBConnection();
+
+let broker = new ServiceBroker();
+
+broker.createService({
+    mixins: [ApiService],
+
+    settings: {
+
+        // Global CORS settings for all routes
+        cors: {
+            // Configures the Access-Control-Allow-Origin CORS header.
+            origin: "*",
+            // Configures the Access-Control-Allow-Methods CORS header.
+            methods: ["GET", "OPTIONS", "POST", "PUT", "DELETE"],
+            // Configures the Access-Control-Allow-Headers CORS header.
+            allowedHeaders: [],
+            // Configures the Access-Control-Expose-Headers CORS header.
+            exposedHeaders: [],
+            // Configures the Access-Control-Allow-Credentials CORS header.
+            credentials: false,
+            // Configures the Access-Control-Max-Age CORS header.
+            maxAge: 3600
+        },
+
+        routes: [{
+            path: "/api",
+
+            // Route CORS settings (overwrite global settings)
+            cors: {
+                origin: ["http://localhost:3701", "https://localhost:4000"],
+                methods: ["GET", "OPTIONS", "POST"],
+                credentials: true
+            },
+        }]
+    }
+});
+
+broker.start();
 
 const express = require('express');
 const cookieParser = require("cookie-parser");
