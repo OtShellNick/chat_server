7422368a9a7c36c0a71997a40c61ea06f7cba35b
otshellnick <exte92@gmail.com>
2022-03-05 19:56:16 +0300

    up auth

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ Server.options('*', cors());
 Server.use('/api/users', require('./requests/users'));
 
 Server.get('/*', auth(), async (req, res) => {
-    const files = (await fs.promises.readdir(`../chat-front/dist`)).map(f => `/${f}`)
+    const files = (await fs.promises.readdir(`../chat-front/dist`)).map(f => `/${f}`);
+    console.log(!files.includes(req.url))
     const file = !files.includes(req.url) ? '/index.html' : req.url;
     const fileStream = fs.createReadStream(path.join(__dirname, `../chat-front/dist${file}`))
     fileStream.pipe(res)
