75379dba47756f4b6ac7a9bbf28428be392b5034
otshellnick <exte92@gmail.com>
2022-03-05 19:30:05 +0300

    up auth

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const path = require('path');
 const fs = require('fs');
+const {auth} = require("./actions/userActions");
 
 const Server = express();
 const {PORT} = process.env;
@@ -16,7 +17,7 @@ Server.options('*', cors());
 
 Server.use('/api/users', require('./requests/users'));
 
-Server.get('/*',async (req, res) => {
+Server.get('/*', auth(), async (req, res) => {
     const files = (await fs.promises.readdir(`../chat-front/dist`)).map(f => `/${f}`)
     const file = !files.includes(req.url) ? '/index.html' : req.url;
     const fileStream = fs.createReadStream(path.join(__dirname, `../chat-front/dist${file}`))
